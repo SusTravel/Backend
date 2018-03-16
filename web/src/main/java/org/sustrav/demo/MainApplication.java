9183f502278fa271389f29a68a7319867fd1aee0
aleksevi <Victor.Alekseev@arhs-developments.com>
2018-03-16 18:50:57 +0100

    - initial project

---
+++ web/src/main/java/org/sustrav/demo/MainApplication.java
@@ -0,0 +1,27 @@
+package org.sustrav.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EnableAutoConfiguration(exclude = {SocialWebAutoConfiguration.class})
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class MainApplication {
+    public static final Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
